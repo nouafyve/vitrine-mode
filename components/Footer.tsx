b283425cae6e5f6b66a07f414b618f9e5e9cb9ef
nouafyve <nacafo@gmail.com>
2025-11-21 15:15:50 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,16 @@
+// components/Footer.tsx
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Link href="#">Mentions légales</Link>
+      <div className="footer-icons">
+        <a href="#" className="footer-icon" />
+        <a href="#" className="footer-icon" />
+        <a href="#" className="footer-icon" />
+      </div>
+    </footer>
+  );
+}
 
